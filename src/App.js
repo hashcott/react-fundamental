41153b18aaaf412b1340164508a0f8900df15f87
fungerouscode <duchanhstyle@gmail.com>
2021-08-22 16:15:53 +0700

    Change background style

--- src/App.js
+++ src/App.js
@@ -2,49 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 // Export component
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // Initial counter
-    this.state = {
-      counter: 0,
-    };
-  }
   render() {
     return (
       <div className="container">
-        <h1
-          onClick={() => {
-            this.setState({
-              counter: 0,
-            });
-          }}
-          className="header"
-        >
-          Counter
-        </h1>
-
-        <p className="number">{this.state.counter}</p>
-
-        <div className="action">
-          <button
-            onClick={() => {
-              this.setState({
-                counter: this.state.counter + 1,
-              });
-            }}
-          >
-            +
-          </button>
-          <button
-            onClick={() => {
-              this.setState({
-                counter: this.state.counter - 1,
-              });
-            }}
-          >
-            -
-          </button>
+        <header>Harry's Todo List</header>
+        <div className="img-svg">
+          <div className="left">
+            <img src="https://mycolor.space/img/color-scheme-left.svg" />
+          </div>
+          <div className="right">
+            <img src="https://mycolor.space/img/color-scheme-right.svg" />
+          </div>
         </div>
       </div>
     );
