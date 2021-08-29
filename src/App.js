a13c4a15ccdd935cdd59e3e214aafbc57bcafcbc
fungerouscode <duchanhstyle@gmail.com>
2021-08-29 15:00:21 +0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+
+import Header from "./components/Header";
 // Export component
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <header>Harry's Todo List</header>
-        <div className="img-svg">
-          <div className="left">
-            <img src="https://mycolor.space/img/color-scheme-left.svg" />
-          </div>
-          <div className="right">
-            <img src="https://mycolor.space/img/color-scheme-right.svg" />
-          </div>
-        </div>
+      <div className="App">
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// react-hooks
+const Header = () => {
+  return <header>Header</header>;
+};
+
+export default Header;
